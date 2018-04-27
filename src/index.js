5405afe05ae99b430c5696e7c2e131d720d6ceb9
J.D. Bean <jonathandbean@gmail.com>
2018-04-27 17:06:03 -0400

    baby steps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // your solution here
-  // grab DOM elements
+  const createListForm = document.querySelector('#create-list-form')
+
+  createListForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let text = document.querySelector('#new-list-title').value
+    // console.log(text)
+    let newList = new List(text);
+  })
+
+
   const listDiv = document.getElementById("app-content");
 
-  const app = new TaskLister();
+  // const app = new TaskLister();
 });
--- src/list.js
+++ src/list.js
@@ -1,3 +1,52 @@
+listId = 0
+
 class List {
-  // your code here
+  constructor(title){
+    this.title = title
+    this.id = listId++
+     // () => {
+    const appContainer = document.getElementById('app-content')
+    const listContainer = document.createElement('div')
+    listContainer.setAttribute("id", "lists")
+    const newList = document.createElement('div')
+    newList.innerHTML = `<h2>${this.title}</h2>`
+    listContainer.appendChild(newList)
+    appContainer.appendChild(listContainer)
+    // }
+
+
+  }
 }
+
+//   <div id="lists">
+// <!-- begin list 1 -->
+//   <div>
+//     <h2>doughnuts
+//       <button data-title="doughnuts" class="delete-list">
+//         X
+//       </button>
+//     </h2>
+//
+//     </ul>
+//   </div>
+// <!--end list 1  -->
+//   // your code here
+// }
+// <ul>
+//     <li>
+//     Task: mocha
+//     <button data-list-title="doughnuts" data-task-name="mocha" class="delete-task">
+//         X
+//     </button>
+//     <br>
+//     Priority: low
+//   </li>
+//   <li>
+//     Task: chocolate
+//     <button data-list-title="doughnuts" data-task-name="chocolate" class="delete-task">
+//       X
+//     </button>
+//     <br>
+//     Priority: high
+//   </li>
+// </ul>
