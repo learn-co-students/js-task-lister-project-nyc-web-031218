4f186a688c987d3479ffe3290b3e61b47cb8d8eb
Helen Liutongco <liutongcoh@gmail.com>
2018-04-28 18:31:41 -0400

    Add index.js code

--- src/list.js
+++ src/list.js
@@ -1,3 +1,16 @@
+const titles = []
+
 class List {
   // your code here
+
+  constructor(title){
+    this.title = title
+    this.tasks = []
+
+    titles.push(title)
+  }
+}
+
+List.titles = function(){
+  return titles
 }
--- src/task.js
+++ src/task.js
@@ -1,3 +1,20 @@
+const descriptions = {}
+
 class Task {
   // your code here
+  constructor(list, description, priority){
+    this.list = list
+    this.description = description
+    if(priority){
+      this.priority = priority
+    } else {
+      this.priority = "low"
+    }
+
+    descriptions[list] = description
+  }
+}
+
+Task.descriptions = function(){
+  return descriptions
 }
